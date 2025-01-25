d865b7d8bf36a1ddfc12c646f43c523eaf1c2258
NihalDixit <nhldxt026@gmail.com>
2025-01-25 16:04:10 +0530

    first react project

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+    return(
+        <header>
+            <h1>My website</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>    
+                </ul>
+
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+
+export default Header
